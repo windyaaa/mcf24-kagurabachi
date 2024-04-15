7b87b529a0187e89985eb0dad2bb2024bc417b1b
ridhopandhu <rdhpndh@gmail.com>
2024-04-15 14:15:22 +0700

    adding warning

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 df = pd.read_csv("data.csv")
+
 
 
 def home():
@@ -33,11 +37,27 @@ def eda():
 
     # st.text("")
     st.markdown(
-        "<h5>20 rows of the dataset</h5>",  
+        "<h5>The head of the data</h5>",  
         unsafe_allow_html=True
     )
     st.write(df.head(20))
+    # Count respondents with high and normal cholesterol levels
+    high_cholesterol_count = df[df['Cholesterol Total (mg/dL)'] >= 200].shape[0]
+    normal_cholesterol_count = df[df['Cholesterol Total (mg/dL)'] < 200].shape[0]
 
+    with st.expander("❗ Pre-indicator for imbalanced data"):
+        # st.markdown('**Pre-indicator for imbalanced data**')
+        st.write(f"Number of respondents with normal cholesterol (< 200 mg/dL): {normal_cholesterol_count}")
+        st.write(f"Number of respondents with high cholesterol (>= 200 mg/dL): {high_cholesterol_count}")
+        
+
+    with st.expander("💡 Variables explanation"):
+        st.write("""
+        ===== TO BE DETERMINED =====
+            """)
+    ##############################
+
+    ##### DESC STATS #####
     st.text("")
     st.markdown(
         "<br>"
@@ -59,7 +79,135 @@ def eda():
             st.write("""
             ===== TO BE DETERMINED =====
                 """)
+    ########################################
 
+
+    ##### DISTRIBUTIONS #####
+
+    st.markdown(
+        "<br>"
+        "<h5>Distributions</h5>",  
+        unsafe_allow_html=True
+    )
+    numerical_columns = [col for col in df.columns if df[col].dtype in ['int64', 'float64'] 
+                            and col not in ['Responden', 'Tempat Lahir']]
+    
+    num_plots = len(numerical_columns)
+    num_cols = 3
+    num_rows = (num_plots + num_cols - 1) // num_cols
+
+    # Define a list of colors
+    colors = sns.color_palette("husl", num_plots)
+
+    for i in range(num_rows):
+        cols = st.columns(num_cols)
+        for j in range(num_cols):
+            idx = i * num_cols + j
+            if idx < num_plots:
+                with cols[j]:
+                    col = numerical_columns[idx]
+                    st.write(col)
+                    sns.histplot(data=df, x=col, kde=True, color=colors[idx])
+                    plt.xlabel('')
+                    st.pyplot()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander("🔍 See explanation"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+    
+    with col2:
+        with st.expander("❓ Why do we use this?"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+
+    #################################
+
+    ##### CORR MATRIX #####
+    st.markdown(
+        "<br>"
+        "<h5>Correlation Matrix</h5>",  
+        unsafe_allow_html=True
+    )
+    correlation_matrix = df[numerical_columns].corr()
+
+    # Generate heatmap
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+    st.pyplot()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander("🔍 See explanation"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+    
+    with col2:
+        with st.expander("❓ Why do we use this?"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+    #################################
+
+
+
+    ##### Tekanan Darah #####
+    st.text("")
+
+    st.markdown(
+        "<br>"
+        "<h5>Blood Pressure Visualization</h5>",  
+        unsafe_allow_html=True
+    )
+
+    # Calculate Mean Arterial Pressure (MAP)
+    df['Mean Arterial Pressure'] = (2 * df['Tekanan darah  (D)'] + df['Tekanan darah  (S)']) / 3
+
+    # Filter respondents based on 'Cholesterol Total (mg/dL)'
+    high_cholesterol_df = df[df['Cholesterol Total (mg/dL)'] >= 200]
+    low_cholesterol_df = df[df['Cholesterol Total (mg/dL)'] < 200]
+
+    # Column 1 for respondents with high cholesterol
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('High Cholesterol')
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=high_cholesterol_df, x="Usia", y="Mean Arterial Pressure", hue="Jenis Kelamin")
+        # plt.title("Mean Arterial Pressure (High Cholesterol)")
+        plt.xlabel("Age")
+        plt.ylabel("Mean Arterial Pressure")
+        plt.legend(title="Gender")
+        st.pyplot()
+
+    # Column 2 for respondents with low cholesterol
+    with col2:
+        st.write('Normal Cholesterol')
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=low_cholesterol_df, x="Usia", y="Mean Arterial Pressure", hue="Jenis Kelamin")
+        # plt.title("Mean Arterial Pressure (Low Cholesterol)")
+        plt.xlabel("Age")
+        plt.ylabel("Mean Arterial Pressure")
+        plt.legend(title="Gender")
+        st.pyplot()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander("🔍 See explanation"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+    
+    with col2:
+        with st.expander("❓ Why do we use this?"):
+            st.write("""
+            ===== TO BE DETERMINED =====
+                """)
+
+    ###############################################
 
 
 # AZHAR'S
